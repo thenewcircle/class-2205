3775551bb748eeea4cb9dc3f746e61418d378d25
Marko Gargenta <marko@marakana.com>
2012-05-30 07:59:34 -0700

    Implemented the activity

--- Fib/src/com/marakana/android/fib/FibActivity.java
+++ Fib/src/com/marakana/android/fib/FibActivity.java
@@ -2,12 +2,31 @@ package com.marakana.android.fib;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class FibActivity extends Activity {
+	EditText in;
+	TextView out;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        in = (EditText)findViewById(R.id.in);
+        out = (TextView)findViewById(R.id.out);
+    }
+    
+    /** Called when the button is clicked. */
+    public void onClick(View v) {
+    		long n = Long.parseLong( in.getText().toString() );
+    		
+    		long resultJ = FibLib.fibJ(n);
+    		out.append( String.format("\nfib(%d)=%d", n, resultJ) );
+    		
+    		long resultN = FibLib.fibN(n);
+    		out.append( String.format("\nfib(%d)=%d", n, resultN) );
     }
 }
